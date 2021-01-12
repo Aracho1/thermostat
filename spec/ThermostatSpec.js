bcdbe6ca01977846ae90e1d3548f73ed3a6b282e
AJ <aj.monty@gmail.com>
2021-01-12 15:33:28 +0000

    added minimum, maximum and power saving mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,7 +7,9 @@ describe('Thermostat', function() {
   describe("'up'", function() {
     it("turns up the temperature", function() {
       let thermostat = new Thermostat();
+      console.log(thermostat.temperature)
       thermostat.turnUp();
+      console.log(thermostat.temperature)
       expect(thermostat.temperature).toEqual(21);
     })
   })
@@ -19,4 +21,39 @@ describe('Thermostat', function() {
       expect(thermostat.temperature).toEqual(19);
     })
   })
+
+  describe("'minimum temperature'", function() {
+    it("has a minimum temperature of 10", function() {
+      let thermostat = new Thermostat();
+      var times = 10;
+      for(var i=0; i <= 10; i++){
+        thermostat.turnDown();
+      }
+      expect(thermostat.temperature).toEqual(10);
+    })
+  })
+  
+  describe("'Maximum temperature'", function() {
+    it("has a max temperature of 25 with PSM on", function() {
+      let thermostat = new Thermostat();
+      var times = 5;
+      for(var i=0; i <= times; i++){
+        thermostat.turnUp();
+      }
+      expect(thermostat.temperature).toEqual(25);
+    })
+  })
+
+  describe("'Maximum temperature'", function() {
+    it("has a max temperature of 32 with PSM off", function() {
+      let thermostat = new Thermostat();
+      thermostat.togglePowerSavingMode()
+      var times = 12;
+      for(var i=0; i <= times; i++){
+        thermostat.turnUp();
+      }
+      expect(thermostat.temperature).toEqual(32);
+    })
+  })
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,13 +1,33 @@
 class Thermostat {
   constructor(){
     this.temperature = 20;
+    this.powerSavingMode = true;
   };
 
   turnUp() {
-    this.temperature++;
-  }
+    if (this.powerSavingMode === true)
+      if (this.temperature < 25)
+        this.temperature++;
+      else
+        this.temperature = 25
+    else if (this.powerSavingMode === false)
+      if (this.temperature < 32)
+        this.temperature++;
+      else
+        this.temperature = 32
+  };
 
   turnDown() {
-    this.temperature--;
-  }
+    if (this.temperature > 10)
+      this.temperature--;
+    else
+      this.temperature = 10;
+  };
+
+  togglePowerSavingMode() {
+    if (this.powerSavingMode === true)
+      return this.powerSavingMode = false
+    else if (this.powerSavingMode === false)
+      return this.powerSavingMode = true
+  };
 };
